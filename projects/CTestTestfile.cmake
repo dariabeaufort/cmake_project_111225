b92d39df04723d0fc978284274d1a0b07962605a
dariabeaufort <zdavydova10@gmail.com>
2025-12-24 02:07:15 +0300

    [cmake project] Remove cmake build files

--- projects/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/zdavy/IT/code/cmake_project_111225/projects
-# Build directory: C:/Users/zdavy/IT/code/cmake_project_111225/projects
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib/googletest")
-subdirs("obuchaika")
-subdirs("sort")
