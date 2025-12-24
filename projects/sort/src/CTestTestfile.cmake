3d74b93ff89a4b9c9d618a2647e23a2a50288726
dariabeaufort <zdavydova10@gmail.com>
2025-12-24 15:04:19 +0300

    [cmake project] All fixed 2

--- projects/sort/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/zdavy/IT/code/cmake_project_111225/projects/sort/src
-# Build directory: C:/Users/zdavy/IT/code/cmake_project_111225/projects/sort/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
